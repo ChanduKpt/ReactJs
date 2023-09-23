2173c6ae81989eacd839ca1fd18b014f0db771f4
ChanduKpt <143503721+ChanduKpt@users.noreply.github.com>
2023-09-23 12:28:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import MessageCard from './Components/MessageCard';
+import WishCard from './Components/WishCard';
+
+let App = () =>{
+  return (
+    <Fragment>
+      <nav className='navbar'>
+        <span>Welcome to App Component </span>
+      </nav>
+      
+    </Fragment>
+  );
+};
+
+export default App;
